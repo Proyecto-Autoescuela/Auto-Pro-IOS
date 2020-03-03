7ca03c27f2a07609aa9847db131fad0c86bad0ea
Proyecto-Autoescuela <proyectoatuoescuelacev@gmail.com>
2020-03-03 20:07:13 +0100

    volver

--- AutoPro-iOS/QuestionsViewController.swift
+++ AutoPro-iOS/QuestionsViewController.swift
@@ -18,8 +18,24 @@ class QuestionsViewController: UIViewController {
     @IBOutlet weak var recargar: UIActivityIndicatorView!
     @IBOutlet var answers: [togglebutton]!
     @IBOutlet weak var tittle: UIButton!
-    
+    @IBOutlet weak var backButton: UIButton!
+    var numberPregunta:Int = 1
+    var arrayAciertos:[Bool] = [true, true, true,true, true, true, true, true, true, true,]
+    @IBOutlet weak var numeroPregunta: UILabel!
     @IBOutlet weak var finaliza: UIButton!
+    
+    
+    @IBAction func volver(_ sender: Any) {
+        questionId = questionId - 1
+        numberPregunta = numberPregunta-1
+        
+        if(!arrayAciertos[questionId]){
+            fallos = fallos-1
+        }else{
+            aciertos = aciertos-1
+        }
+        reloadData()
+    }
     @IBAction func finalizar(_ sender: Any) {
         if(buttonA.isOn || buttonB.isOn || buttonC.isOn){
             
@@ -53,23 +69,31 @@ class QuestionsViewController: UIViewController {
             if(buttonA.isOn){
                 if(questions[questionId].correctAnswer.rawValue == "answer_a"){
                     aciertos = aciertos+1
+                    arrayAciertos[questionId] = true
                 }else{
                     fallos = fallos+1
+                    arrayAciertos[questionId] = false
                 }
             }else if(buttonB.isOn){
                 if(questions[questionId].correctAnswer.rawValue == "answer_b"){
                     aciertos = aciertos+1
+                    arrayAciertos[questionId] = true
                 }else{
                     fallos = fallos+1
+                    arrayAciertos[questionId] = false
                 }
             }else{
                 if(questions[questionId].correctAnswer.rawValue == "answer_c"){
+                    arrayAciertos[questionId] = true
                     aciertos = aciertos+1
                 }else{
                     fallos = fallos+1
+                    arrayAciertos[questionId] = false
                 }
             }
             questionId = questionId+1
+            numberPregunta = numberPregunta+1
+            
             reloadData()
         }
         print(aciertos, fallos)
@@ -83,9 +107,15 @@ class QuestionsViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         finaliza.isHidden = true
+        backButton.isHidden = true
         finaliza.isEnabled = false
+        questionId = 0
+        fallos = 0
+        aciertos = 0
         reloadData()
         recargar.stopAnimating()
+        numberPregunta = 1
+        numeroPregunta.text = String(numberPregunta)
     }
     
     private func reloadData(){
@@ -101,6 +131,11 @@ class QuestionsViewController: UIViewController {
         answerA.text = questions[questionId].answerA
         answerB.text = questions[questionId].answerB
         answerC.text = questions[questionId].answerC
+        numeroPregunta.text = String(numberPregunta)
+        backButton.isHidden = false
+        if(questionId==0){
+            backButton.isHidden = true
+        }
         let baseURL = URL(string: "http://localhost:8888/autopro/AutoPro-API-features-migrations/storage/app/public/")!
         let placeholderImage = UIImage(named: "autoescuela-logo.png")
         let remoteImageURL = baseURL.appendingPathComponent(questions[questionId].photoURL)
