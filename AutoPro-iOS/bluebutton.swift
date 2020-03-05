3300e0d894830e5344abd84307bdab807a7269a8
Proyecto-Autoescuela <proyectoatuoescuelacev@gmail.com>
2020-03-05 18:50:35 +0100

    no message

--- AutoPro-iOS/bluebutton.swift
+++ AutoPro-iOS/bluebutton.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 struct Colors {
-    static let bluebutton = UIColor(red: 29.0/255.0, green: 161.0/255.0, blue: 242.0/255.0, alpha: 1.0)
-    static let greenbutton = UIColor(red: 0.0/255.0, green: 255.0/255.0, blue: 0.0/255.0, alpha: 1.0)
-    static let redbutton = UIColor(red: 255.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    static let bluebutton = UIColor(red: 19.0/255.0, green: 41.0/255.0, blue: 61.0/255.0, alpha: 1.0)
+    static let greenbutton = UIColor(red: 27.0/255.0, green: 168.0/255.0, blue: 72.0/255.0, alpha: 1.0)
+    static let redbutton = UIColor(red: 199.0/255.0, green: 20.0/255.0, blue: 24.0/255.0, alpha: 1.0)
 }
